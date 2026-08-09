a3b3599864c1e67d14d686f9543db7640ebbb34b
Sara Othman <sarashehadi17@gmail.com>
2026-08-10 00:07:47 +0300

    add new daftar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ShoppingProvider } from "./contexts/ShoppingContext.jsx";
 import { RecipeProvider } from "./contexts/RecipeContext.jsx";
+import { NotebookProvider } from "./contexts/NotebookContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
@@ -9,12 +10,14 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RecipeProvider>
-      <ShoppingProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ShoppingProvider>
-    </RecipeProvider>
+    <NotebookProvider>
+      <RecipeProvider>
+        <ShoppingProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ShoppingProvider>
+      </RecipeProvider>
+    </NotebookProvider>
   </StrictMode>,
 );
